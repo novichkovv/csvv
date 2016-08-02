d884763ec5a17663a190f63b739fb1f47887afbd
novichkovv <novichkovv@bk.ru>
2016-08-02 13:10:42 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/config.sample.php
+++ www/config.sample.php
@@ -2,21 +2,32 @@
 /**
  * configuration file
  */
-
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_DIR', realpath($_SERVER['DOCUMENT_ROOT']) . DS);
 define('CORE_DIR', ROOT_DIR . 'core' . DS);
-define('SITE_DIR', 'http://' . str_replace('http://', '', $_SERVER['HTTP_HOST'] . '/'));
+$site_dir = 'http://' . str_replace('http://', '', $_SERVER['HTTP_HOST'] . '/');
+$project = 'frontend';
+if(!empty($_GET['route'])) {
+    $route = trim($_GET['route'], '\\/');
+    $parts = explode('?', $route);
+    $arr = explode('/', $parts[0]);
+    if($arr[0] == 'backend') {
+        if(count($arr) > 1) {
+            unset($arr[0]);
+            $_GET['route'] = implode('/', $arr) . '/';
+        } else {
+            unset($_GET['route']);
+        }
+        $project = 'backend';
+    } else {
+        $project = 'frontend';
+    }
+}
+
+define('SITE_DIR', $site_dir);
 if(count($arr = explode('.', $_SERVER['HTTP_HOST'])) > 2) {
     $sub_domain = array_shift($arr);
-    $project = in_array($sub_domain, array(
-        'www',
-        'dev'
-    )) ? 'frontend' : $sub_domain;
-
-} else {
-    $project = 'frontend';
 }
 define('MAIN_SITE_DIR', 'http://' . str_replace('http://', '', implode('.', $arr) . '/'));
 define('PROJECT', $project);
@@ -25,6 +36,7 @@ define('CONTROLLER_DIR', ROOT_DIR . 'controllers' . DS . PROJECT . DS);
 define('LIBS_DIR', ROOT_DIR . 'libs' . DS);
 define('IMAGE_DIR', SITE_DIR . '/images/' . PROJECT . '/');
 define('DEVELOPMENT_MODE', true);
+
 
 
 define('DB_NAME', 'csv');
